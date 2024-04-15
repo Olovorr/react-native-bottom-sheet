0421fbf7eb11f629270236ec6f7e405e8095fb2b
Oliver Rýdzi <orydzi@gmail.com>
2024-04-15 13:22:00 +0200

    fix: Fixed condition for opacity in BottomSheer so it is always 1 for android devices.

--- src/components/bottomSheet/BottomSheet.tsx
+++ src/components/bottomSheet/BottomSheet.tsx
@@ -1176,7 +1176,7 @@ const BottomSheetComponent = forwardRef<BottomSheet, BottomSheetProps>(
     const containerAnimatedStyle = useAnimatedStyle(
       () => ({
         opacity:
-          Platform.OS === 'android' && animatedIndex.value === -1 ? 0 : 1,
+          Platform.OS === 'ios' && animatedIndex.value === -1 ? 0 : 1,
         transform: [
           {
             translateY: animatedPosition.value,
